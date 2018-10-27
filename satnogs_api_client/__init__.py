cc1b01958a440f48075ba4365f0dad883faddd9b
Fabian P. Schmidt <kerel-fs@gmx.de>
2018-10-27 17:44:51 +0200

    Fix coding style & linting errors

--- satnogs_api_client/__init__.py
+++ satnogs_api_client/__init__.py
@@ -1,10 +1,18 @@
+# flake8: noqa
 """
 SatNOGS API client module initialization
 """
 from __future__ import absolute_import
 
 from ._version import get_versions
-from .satnogs_api_client import *
+from .satnogs_api_client import fetch_observation_data_from_id, \
+                                fetch_observation_data, \
+                                fetch_ground_station_data, \
+                                fetch_satellite_data, \
+                                fetch_tle_of_observation, \
+                                fetch_telemetry, \
+                                fetch_transmitters, \
+                                post_telemetry
 
 __version__ = get_versions()['version']
 
--- satnogs_api_client/satnogs_api_client.py
+++ satnogs_api_client/satnogs_api_client.py
@@ -1,5 +1,7 @@
-import requests
+from __future__ import print_function
 import re
+
+import requests
 
 
 NETWORK_DEV_BASE_URL = 'https://network-dev.satnogs.org'
@@ -26,10 +28,12 @@ def get_paginated_endpoint(url):
 
 
 def fetch_observation_data_from_id(norad_id, start, end, prod=True):
-    # Get all observations of the satellite with the given `norad_id` in the given timeframe
+    # Get all observations of the satellite
+    # with the given `norad_id` in the given timeframe
     # https://network.satnogs.org/api/observations/?satellite__norad_cat_id=25544&start=2018-06-10T00:00&end=2018-06-15T00:00
 
-    query_str = '{}/api/observations/?satellite__norad_cat_id={}&start={}&end={}'
+    query_str = '{}/api/observations/' \
+                '?satellite__norad_cat_id={}&start={}&end={}'
 
     url = query_str.format(NETWORK_BASE_URL if prod else NETWORK_DEV_BASE_URL,
                            norad_id,
@@ -38,19 +42,22 @@ def fetch_observation_data_from_id(norad_id, start, end, prod=True):
 
     observations = get_paginated_endpoint(url)
 
-    # Current prod is broken and can't filter on NORAD ID correctly, use client-side filtering instead
-    observations = list(filter(lambda o: o['norad_cat_id'] == norad_id, observations))
+    # Current prod is broken and can't filter on NORAD ID correctly,
+    # use client-side filtering instead
+    observations = list(observation for observation in observations
+                        if observation['norad_cat_id'] == norad_id)
 
     return observations
 
 
 def fetch_observation_data(observation_ids, prod=True):
     # Get station location from the observation via the observation_id
-    
+
     observations = []
     for observation_id in observation_ids:
-        r = requests.get(url='{}/api/observations/{}/'.format(NETWORK_BASE_URL if prod else NETWORK_DEV_BASE_URL,
-                                                          observation_id))
+        base_url = (NETWORK_BASE_URL if prod else NETWORK_DEV_BASE_URL)
+        r = requests.get(url='{}/api/observations/{}/'.format(base_url,
+                                                              observation_id))
         if r.status_code != requests.codes.ok:
             print("Observation {} not found in network.".format(observation_id))
             continue
@@ -58,24 +65,26 @@ def fetch_observation_data(observation_ids, prod=True):
 
     return observations
 
+
 def fetch_ground_station_data(ground_station_ids, prod=True):
     # Fetch ground station metadata from network
     ground_stations = []
     for ground_station_id in ground_station_ids:
-        
         # Skip frames from deleted groundstations, indidcated as ID 'None'
         if str(ground_station_id) == 'None':
             print("Skipping groundstation 'None'.")
             continue
-        
-        r = requests.get(url='{}/api/stations/{}/'.format(NETWORK_BASE_URL if prod else NETWORK_DEV_BASE_URL,
-                                                      ground_station_id))
+
+        base_url = (NETWORK_BASE_URL if prod else NETWORK_DEV_BASE_URL)
+        r = requests.get(url='{}/api/stations/{}/'.format(base_url,
+                                                          ground_station_id))
+
         if r.status_code != requests.codes.ok:
             print("Ground Station {} not found in db.".format(ground_station_id))
             raise
-        data = r.json()
         ground_stations.append(r.json())
     return ground_stations
+
 
 def fetch_satellite_data(norad_cat_id):
     # Fetch satellite metadata from network
@@ -85,8 +94,10 @@ def fetch_satellite_data(norad_cat_id):
 
     return r.json()
 
+
 def fetch_tle_of_observation(observation_id, prod=True):
-    url = '{}/observations/{}/'.format(NETWORK_BASE_URL if prod else NETWORK_DEV_BASE_URL,
+    base_url = (NETWORK_BASE_URL if prod else NETWORK_DEV_BASE_URL)
+    url = '{}/observations/{}/'.format(base_url,
                                        observation_id)
     r = requests.get(url=url)
     observation_page_html = r.text
@@ -100,7 +111,7 @@ def fetch_tle_of_observation(observation_id, prod=True):
     return [obs_tle_2, obs_tle_3]
 
 
-def fetch_telemetry(norad_id, max_frames, url):
+def fetch_telemetry(norad_id, url):
     # http://db-dev.satnogs.org/api/telemetry/?satellite=43595
 
     query_str = '{}/api/telemetry/?satellite={}'
@@ -124,7 +135,7 @@ def fetch_transmitters(norad_id, url):
 
 
 def post_telemetry(norad_id,
-                   source, # Receiver Callsign
+                   source,  # Receiver Callsign
                    lon,
                    lat,
                    timestamp,
