e20a7d3c98526edb55f0dd20f58a4295f09a7bff
Fabian P. Schmidt <kerel-fs@gmx.de>
2018-10-28 16:11:28 +0100

    Export base urls

--- satnogs_api_client/__init__.py
+++ satnogs_api_client/__init__.py
@@ -13,7 +13,11 @@ from .satnogs_api_client import fetch_observation_data_from_id, \
                                 fetch_telemetry, \
                                 fetch_transmitters, \
                                 fetch_satellites, \
-                                post_telemetry
+                                post_telemetry, \
+                                NETWORK_DEV_BASE_URL, \
+                                NETWORK_BASE_URL, \
+                                DB_DEV_BASE_URL, \
+                                DB_BASE_URL
 
 __version__ = get_versions()['version']
 
